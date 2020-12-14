f89bf3b4518f3892e3889ad02df7ebd65e62147d
techken <globaljack529@gmail.com>
2020-12-15 01:13:55 +0900

    [AC] abc175 abc175_c

---
+++ atcoder.jp/abc175/abc175_c/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll X, K, D;
+    cin >> X >> K >> D;
+    X = abs(X);
+    ll straight = min(K, X / D);
+    K -= straight;
+    X -= straight * D;
+    if (K % 2 == 0) {
+        cout << X << endl;
+    } else {
+        cout << D - X << endl;
+    }
+    return 0;
+}
